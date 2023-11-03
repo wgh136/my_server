8d3e52d6b2107ef4ba1cf32a7a818a34509187ac
wgh136 <wgh1624044369@gmail.com>
2023-11-03 13:20:38 +0800

    update bot

--- internal/tg_bot.go
+++ internal/tg_bot.go
@@ -1,17 +1,16 @@
 package internal
 
 import (
+	json2 "encoding/json"
+	"io"
 	"my_server/pkg/tg_bot"
+	"os"
 	"strconv"
 	"strings"
 )
 
-var (
-	userNameToChatID = map[string]int{}
-)
-
 func OnUpdate(update tg_bot.Update) {
-	userNameToChatID[update.Message.From.Username] = update.Message.Chat.ID
+	addData(update.Message.From.Username, update.Message.Chat.ID)
 	if *update.Message.Text == "/start" {
 		tg_bot.SendMessage("Welcome!\nYou can send me message.", update.Message.Chat.ID)
 		return
@@ -21,7 +20,7 @@ func OnUpdate(update tg_bot.Update) {
 		if (*update.Message.Text)[0] == '@' {
 			username := strings.Split(*update.Message.Text, "\n")[0]
 			username = username[1:]
-			chatId := userNameToChatID[username]
+			chatId := readData()[username]
 			tg_bot.SendMessage(strings.Replace(*update.Message.Text, "@"+username+"\n", "", 1), chatId)
 		}
 		tg_bot.SendMessage("ok", meChatId)
@@ -31,4 +30,34 @@ func OnUpdate(update tg_bot.Update) {
 		tg_bot.ForwardMessage(update.Message, meChatId)
 		tg_bot.SendMessage("@"+update.Message.From.Username, meChatId)
 	}
+}
+
+func readData() map[string]int {
+	file, err := os.Open(resourcesPath + string(os.PathSeparator) + "charIDs.json")
+	if err != nil {
+		return map[string]int{}
+	}
+	defer file.Close()
+	var res map[string]int
+	data, err := io.ReadAll(file)
+	if err != nil {
+		return map[string]int{}
+	}
+	json2.Unmarshal(data, &res)
+	return res
+}
+
+func addData(userName string, chatID int) {
+	data := readData()
+	data[userName] = chatID
+	file, err := os.Open(resourcesPath + string(os.PathSeparator) + "charIDs.json")
+	defer file.Close()
+	if err != nil {
+		return
+	}
+	bytes, err := json2.Marshal(data)
+	if err != nil {
+		return
+	}
+	file.Write(bytes)
 }
